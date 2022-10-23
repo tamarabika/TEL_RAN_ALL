3077953797fb9c1e498a03f0bf5f55aaf4b87d2a
tamarabika <tamara.bikashova@gmail.com>
2022-10-23 09:58:41 +0200

    new

---
+++ GenTech/Homework/Lesson1/Roborock S7.js
@@ -0,0 +1,9 @@
+db.orders.insertOne({
+    'date':'22.02.2022',
+    'id':'gr5t5',
+    'title':'Roborock S7',
+    'category':'Robot vacuum cleaners',
+    'brand':'Roborock',
+    'mk.':'35343657978',
+    'pice':'489 Euro'
+    })
---
+++ Python/Lesson 1/from cmath import phase.py
@@ -0,0 +1,13 @@
+car = {
+    'price':100,
+    'model': '...'
+    'brand':'...'
+    'title':'Автомобиль какой-то'
+    'store_id':'s5'
+    'datetime': {
+        'created':'...'
+    }
+
+}
+car['price'] = 123
+print(car ['price'])
